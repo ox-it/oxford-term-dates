4447111ed83ac476ad63cd3fe2ac670eb0b68969
Chris Northwood <christopher.northwood@oucs.ox.ac.uk>
2011-03-03 16:59:01 +0000

    Add a setup script

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Distutils',
+      version='1.0',
+      description='Oxford University Computing Services',
+      author='Oxford University Computing Services',
+      author_email='mobileoxford@oucs.ox.ac.uk',
+      url='https://github.com/oucs/oxford-term-dates',
+      packages=['oxford_term_dates'],
+     )
