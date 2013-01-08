d7a0dc7a7a3e785d8d3e850b88aa68af3dd34ab2
David King <dave@davbo.org>
2013-01-08 10:35:55 +0000

    Added 2013-2014 dates and pushed version up.
    See note about one of the dates falling on Easter Sunday

--- oxford_term_dates/__init__.py
+++ oxford_term_dates/__init__.py
@@ -46,6 +46,9 @@ TERM_STARTS = {
     (2012, 3): date(2012, 9, 30),
     (2013, 1): date(2013, 1, 6),
     (2013, 2): date(2013, 4, 14),
+    (2013, 3): date(2013, 10, 6),
+    (2014, 1): date(2014, 1, 12),
+    (2014, 2): date(2014, 4, 20),  # Easter Sunday?
 }
 
 OFFSET_TERM_STARTS = dict((k,v-timedelta(14)) for k,v in TERM_STARTS.items())
